63f1a19ffcdb44c09943f14f9175954aca0f936f
kylelovestoad <kylerichards32@gmail.com>
2020-05-17 01:28:28 -0400

    Updated ExpandCommand.java to send a message when no args are passed

--- src/main/java/com/kylelovestoad/kylebot/command/commands/ExpandCommand.java
+++ src/main/java/com/kylelovestoad/kylebot/command/commands/ExpandCommand.java
@@ -2,19 +2,32 @@ package com.kylelovestoad.kylebot.command.commands;
 
 import com.kylelovestoad.kylebot.command.CommandCategory;
 import com.kylelovestoad.kylebot.command.ICommand;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
+import java.awt.*;
 import java.util.List;
 
 public class ExpandCommand implements ICommand {
     @Override
     public void handle(GuildMessageReceivedEvent event, List<String> args) {
 
+        TextChannel channel = event.getChannel();
+
         if (args.isEmpty()) {
+
+            final EmbedBuilder embed = new EmbedBuilder()
+                    .setTitle("This is the saddest display I've ever seen")
+                    .setDescription("❌ You have to actually put text to expand")
+                    .setFooter("Bro")
+                    .setColor(Color.RED);
+
+            channel.sendMessage(embed.build()).queue();
             return;
         }
 
-        event.getChannel().sendMessage(String.join(" ", args.subList(0,args.size())).replace("", " ").trim()).queue();
+        channel.sendMessage(String.join(" ", args.subList(0,args.size())).replace("", " ").trim()).queue();
     }
 
     @Override
