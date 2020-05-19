5eb69192421c082ace25a8ff1fa1fbe44e253bfb
kylelovestoad <kylerichards32@gmail.com>
2020-05-19 18:49:00 -0400

    Updated Config.java to use .properties

--- src/main/java/com/kylelovestoad/kylebot/Config.java
+++ src/main/java/com/kylelovestoad/kylebot/Config.java
@@ -1,16 +1,42 @@
 package com.kylelovestoad.kylebot;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Config {
 
-    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final Properties p = new Properties();
+
+    private static FileReader reader;
+
+    private static FileOutputStream fos;
+
 
     /**
-     * @param key The key which gets the environment variable
-     * @return The value of an environment variable
+     * @param key The key which gets the property
+     * @return The value of a property
      */
     public static String get(String key) {
-        return dotenv.get(key.toUpperCase());
+        try {
+            reader = new FileReader("config.properties");
+            p.load(reader);
+        } catch(IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return p.getProperty(key.toUpperCase());
+    }
+
+    public static void add(String key, String value) {
+        p.setProperty(key.toUpperCase(), value);
+        try {
+            p.store(new FileWriter("config.properties"), "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
