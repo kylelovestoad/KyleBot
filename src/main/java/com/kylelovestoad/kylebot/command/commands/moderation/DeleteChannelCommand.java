c2f94d8a0c0ca1c773af53fbbd0398a10a2c942d
kylelovestoad <kylerichards32@gmail.com>
2020-06-01 00:57:22 -0400

    Added DeleteChannelCommand

---
+++ src/main/java/com/kylelovestoad/kylebot/command/commands/moderation/DeleteChannelCommand.java
@@ -0,0 +1,68 @@
+package com.kylelovestoad.kylebot.command.commands.moderation;
+
+import com.kylelovestoad.kylebot.command.CommandCategory;
+import com.kylelovestoad.kylebot.command.ICommand;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.EnumSet;
+import java.util.List;
+
+public class DeleteChannelCommand implements ICommand {
+    @Override
+    public void handle(GuildMessageReceivedEvent event, List<String> args) {
+
+        TextChannel channel = event.getChannel();
+        Guild guild = event.getGuild();
+
+        if (args.isEmpty()) {
+            channel.delete().queue();
+            return;
+        }
+
+        if (args.size() == 1) {
+            guild.getChannels().stream().filter(c -> c.getName().equals(args.get(0))).forEach(c -> c.delete().queue());
+            return;
+        }
+
+        guild.getCategoriesByName(args.get(1), true).forEach(category -> category.getChannels().stream().filter(c -> c.getName().equals(args.get(0))).forEach(c -> c.delete().queue()));
+
+    }
+
+    @Override
+    public String getName() {
+        return "deletechannel";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Deletes any channels with a specified name. Deletes the channel you send the command in if no arguments are present";
+    }
+
+    @Override
+    public String getUsage() {
+        return "<channel name> [category]";
+    }
+
+    @Override
+    public boolean isOwnerCommand() {
+        return false;
+    }
+
+    @Override
+    public CommandCategory getCategory() {
+        return CommandCategory.MODERATION;
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return List.of("delchannel", "removechannel", "rmchannel");
+    }
+
+    @Override
+    public EnumSet<Permission> getPermissions() {
+        return EnumSet.of(Permission.MANAGE_CHANNEL);
+    }
+}
